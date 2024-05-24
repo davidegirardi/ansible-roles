a81383bf1be456a0ec833773be6ed0f0d414cf91
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2024-05-24 09:04:16 +0200

    Improve login detection

--- tunnel_user/files/tunnel_one_time_password.sh
+++ tunnel_user/files/tunnel_one_time_password.sh
@@ -10,17 +10,8 @@ echo Setting the following password to "$user"
 printf "$(pwgen 10 1)\n%.0s" {1..2} | tee >(passwd "$user" &>/dev/null) | tail -n 1
 
 echo Waiting for the user "$user" to login...
-while true
-do
-    if ps -U"$user" &>/dev/null
-    then
-        break
-    fi
-    sleep 1
-done
+journalctl -f -n0 -u ssh.service | grep -q "user $user"
 
-#echo Setting the following password to "$user"
-#printf "$(pwgen -s 32 1)\n%.0s" {1..2} | tee >(passwd "$user" &>/dev/null) | tail -n 1
 echo "Running processes for $user"
 echo Status:
 ps -U"$user"
