5e685a034e6ea7962bd302fd05f8c0e10aa8978a
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2025-11-03 10:03:17 +0100

    Updates for Debian 13

--- tunnel_user/files/tunnel_one_time_password.sh
+++ tunnel_user/files/tunnel_one_time_password.sh
@@ -10,7 +10,7 @@ echo Setting the following password to "$user"
 printf "$(pwgen 10 1)\n%.0s" {1..2} | tee >(passwd "$user" &>/dev/null) | tail -n 1
 
 echo Waiting for the user "$user" to login...
-journalctl -f -n0 -u ssh.service | grep -q "user $user"
+journalctl -f -n0 -u sshd.service | grep -q "user $user"
 
 echo "Running processes for $user"
 echo Status:
